11127be15f9d909c9cdde8555281a6d577909230
GhadeerAlbattarni <ghadeeralbattarni@gmail.com>
2024-09-02 21:04:24 -0400

    Implement some features on cart, user, menu with Redux toolkit

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,9 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
 
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  // console.log(currentQuantity);
 
+  const isInCart = currentQuantity > 0;
+
+  function handleAddItemToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+
+    dispatch(addItem(newItem));
+  }
   return (
     <li className="flex gap-4 py-2">
       <img
@@ -24,7 +43,12 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type="small">Add to cart</Button>
+          {isInCart && <DeleteItem pizzaId={id} />}
+          {!soldOut && !isInCart && (
+            <Button type="small" onClick={handleAddItemToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
